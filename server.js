9ba3404c1999f40ea1eb780413621cd0768933ee
surya2312 <suryaprasad231@gmail.com>
2017-04-16 23:00:25 -0500

    cors changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
